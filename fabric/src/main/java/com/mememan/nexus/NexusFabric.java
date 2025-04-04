b9ad82b7697087b4d5d49d0f55768a034de368d9
Meme Man <81907436+RaveTr@users.noreply.github.com>
2025-04-04 20:08:46 +0200

    Remove unnecessary dev logging

--- fabric/src/main/java/com/mememan/nexus/NexusFabric.java
+++ fabric/src/main/java/com/mememan/nexus/NexusFabric.java
@@ -1,6 +1,5 @@
 package com.mememan.nexus;
 
-import com.mememan.nexus.platform.NexusServices;
 import net.fabricmc.api.ModInitializer;
 
 public class NexusFabric implements ModInitializer {
@@ -8,10 +7,5 @@ public class NexusFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         Nexus.initialize();
-
-        NexusServices.PLATFORM_MANAGER.getModData().forEach(d -> {
-            NexusConstants.LOGGER.info("Mod {} has dependencies: {}", d.getModMetadata().modId(), d.getModMetadata().modDependencies());
-            NexusConstants.LOGGER.info("Mod {} has paths: {}", d.getModMetadata().modId(), d.getAllFilePaths());
-        });
     }
 }
--- forge/src/main/java/com/mememan/nexus/NexusForge.java
+++ forge/src/main/java/com/mememan/nexus/NexusForge.java
@@ -1,6 +1,5 @@
 package com.mememan.nexus;
 
-import com.mememan.nexus.platform.NexusServices;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(NexusConstants.MOD_ID)
@@ -8,10 +7,5 @@ public class NexusForge {
     
     public NexusForge() {
         Nexus.initialize();
-
-        NexusServices.PLATFORM_MANAGER.getModData().forEach(d -> {
-            NexusConstants.LOGGER.info("Mod {} has dependencies: {}", d.getModMetadata().modId(), d.getModMetadata().modDependencies());
-            NexusConstants.LOGGER.info("Mod {} has paths: {}", d.getModMetadata().modId(), d.getAllFilePaths());
-        });
     }
 }
