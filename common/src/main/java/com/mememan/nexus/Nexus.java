eab263e5c192bbabd7be914fc8beee4d76498343
Meme Man <81907436+RaveTr@users.noreply.github.com>
2025-04-05 00:40:19 +0200

    Networking correction

--- common/src/main/java/com/mememan/nexus/Nexus.java
+++ common/src/main/java/com/mememan/nexus/Nexus.java
@@ -1,11 +1,27 @@
 package com.mememan.nexus;
 
+import com.mememan.nexus.loader.ModLoader;
+import com.mememan.nexus.platform.NexusServices;
+
 /**
  * Core class for initializing Nexus API.
  */
 public class Nexus {
 
+    /**
+     * Central initialization method for Nexus API.
+     */
     public static void initialize() {
 
+
+        if (NexusServices.PLATFORM_MANAGER.getPlatform().equals(ModLoader.FABRIC)) NexusServices.NETWORK_MANAGER.setupNetworkHandler();
+    }
+
+    /**
+     * Initialization method for startup tasks that need to be deferred to a later mod-loading stage, primarily on
+     * Neo/Forge.
+     */
+    public static void initializeDeferred() {
+        NexusServices.NETWORK_MANAGER.setupNetworkHandler();
     }
 }
--- forge/src/main/java/com/mememan/nexus/NexusForge.java
+++ forge/src/main/java/com/mememan/nexus/NexusForge.java
@@ -1,11 +1,21 @@
 package com.mememan.nexus;
 
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod(NexusConstants.MOD_ID)
+@Mod.EventBusSubscriber
 public class NexusForge {
     
     public NexusForge() {
         Nexus.initialize();
+    }
+
+    @SubscribeEvent
+    public static void onFMLCommonSetupEvent(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() ->
+                Nexus.initializeDeferred()
+        );
     }
 }
